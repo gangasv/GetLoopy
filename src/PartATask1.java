2d02cc9fe074d810720b34410ab1a2540aa115f1
gangasv <gangasv@mail.uc.edu>
2026-06-27 11:15:21 -0400

    Part A Task 1 Complete

---
+++ src/PartATask1.java
@@ -0,0 +1,8 @@
+public class PartATask1 {
+    public static void main(String[] args) {
+        // Counts up by 1 from 0 to 30
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
