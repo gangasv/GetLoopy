d57af0b1b290276131a5aba30fc46caf8c4729bb
gangasv <gangasv@mail.uc.edu>
2026-06-27 23:14:42 -0400

    Part A Task 4 complete

--- src/PartATask2.java
+++ src/PartATask2.java
@@ -1,6 +1,6 @@
 public class PartATask2 {
     public static void main(String[] args) {
-        // Counts down by 1 from 0 to 30
+        // Counts down by 1 from 30 to 0
         for (int i = 30; i >= 0; i--) {
             System.out.print(i + " ");
         }
---
+++ src/PartATask4.java
@@ -0,0 +1,8 @@
+public class PartATask4 {
+    public static void main(String[] args) {
+        //for loop that counts down by 2 from 10 to 0
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
