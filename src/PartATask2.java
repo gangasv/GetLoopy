3eaacbeb30c37d46f6c611f1e7f2ac6a16375791
gangasv <gangasv@mail.uc.edu>
2026-06-27 23:25:26 -0400

    Part B Task 6 complete

--- src/PartATask2.java
+++ src/PartATask2.java
@@ -1,6 +1,6 @@
 public class PartATask2 {
     public static void main(String[] args) {
-        // Counts down by 1 from 30 to 0
+        // For loop that counts down by 1 from 30 to 0
         for (int i = 30; i >= 0; i--) {
             System.out.print(i + " ");
         }
---
+++ src/PartBTask7.java
@@ -0,0 +1,11 @@
+public class PartBTask7 {
+    public static void main(String[] args) {
+        // Nested for loop to create triangle of asterisks
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= 5; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
