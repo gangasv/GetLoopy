af7705aa1c5185ca45e027ce04aea56018b39467
gangasv <gangasv@mail.uc.edu>
2026-06-27 23:26:11 -0400

    Part B Task 7 complete

--- src/PartBTask7.java
+++ src/PartBTask7.java
@@ -1,6 +1,6 @@
 public class PartBTask7 {
     public static void main(String[] args) {
-        // Nested for loop to create triangle of asterisks
+        // Nested for loop to create asterisks
         for (int row = 1; row <= 5; row++) {
             for (int col = 1; col <= 5; col++) {
                 System.out.print("* ");
