e022108cda8f271b00e47023703e2e4ecaab11bb
gangasv <gangasv@mail.uc.edu>
2026-06-27 23:10:37 -0400

    Part A Task 3 complete

---
+++ src/PartATask3.java
@@ -0,0 +1,8 @@
+public class PartATask3 {
+    public static void main(String[] args) {
+        // for loop that counts up by 3 from 0 to 18
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.print(i + " ");
+        }
+    }
+}
