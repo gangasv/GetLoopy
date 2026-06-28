eaae30299535c31ebc201cf4f3b6d6027f68c924
gangasv <gangasv@mail.uc.edu>
2026-06-27 23:07:08 -0400

    Part A Task 2 Complete

---
+++ src/PartATask2.java
@@ -0,0 +1,8 @@
+public class PartATask2 {
+    public static void main(String[] args) {
+        // Counts down by 1 from 0 to 30
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
